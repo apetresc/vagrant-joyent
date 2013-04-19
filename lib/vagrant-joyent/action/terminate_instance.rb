830bc3e6753e33ababaa304be10b645c10472103
Sean OMeara <someara@opscode.com>
2013-04-19 08:53:42 -0400

    adding loop to terminate to wait for full destruction before proceeding

--- lib/vagrant-joyent/action/terminate_instance.rb
+++ lib/vagrant-joyent/action/terminate_instance.rb
@@ -15,10 +15,20 @@ module VagrantPlugins
 
           # Destroy the server and remove the tracking ID
           env[:ui].info(I18n.t("vagrant_joyent.terminating"))
-          server.stop
-          server.destroy
+          server.destroy        
           env[:machine].id = nil
 
+          # Wait for server to be completely gone from invetory
+          while true do
+            ids = []
+            env[:joyent_compute].servers.collect.each { |s|
+              ids << s.id
+            }
+            unless ids.include?(env[:machine].id) then
+              break
+            end
+          end
+          
           @app.call(env)
         end
       end
